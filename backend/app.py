ac1e13db2414a8a7cbc0a0aa6c61907870b23332
tolaoluniyi <170970335+tolaoluniyi@users.noreply.github.com>
2025-02-15 19:50:47 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,12 @@
+from flask import Flask, jsonify
+from predictor import predict_matches
 
+app = Flask(__name__)
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    predictions = predict_matches()
+    return jsonify(predictions.to_dict(orient='records'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
